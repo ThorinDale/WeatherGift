237790347bb7b113a8fcce10d5d0088081b29cf0
Darren Zheng <zhengdarren9@gmail.com>
2021-03-20 16:10:19 -0400

    with .gitignore

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Darren Zheng on 3/20/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
